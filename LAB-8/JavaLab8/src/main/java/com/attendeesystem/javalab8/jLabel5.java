7b725670a2ecaee805cd0414d2c69c2aad554ddb
Mehakk <lunkarmehakk@gmail.com>
2025-01-05 11:41:24 +0530

    Add files via upload

---
+++ LAB-8/JavaLab8/src/main/java/com/attendeesystem/javalab8/jLabel5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.attendeesystem.javalab8;
+
+import java.awt.Font;
+
+/**
+ *
+ * @author mehakklunkar
+ */
+class jLabel5 {
+
+    static void setFont(Font font) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setText(String country) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
